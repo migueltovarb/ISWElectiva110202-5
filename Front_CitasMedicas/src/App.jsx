cbb0576cc86df7239fa6d3b875e6e3e8439030a4
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-04-24 00:46:00 -0500

    crear medico

--- Front_CitasMedicas/src/App.jsx
+++ Front_CitasMedicas/src/App.jsx
@@ -1,11 +1,14 @@
-import CrearUsuario from "./components/CrearUsuario";
+import RegistrarPaciente from './components/CrearUsuario';
+import RegistrarMedico from './components/CrearMedico';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-10">
-      <CrearUsuario/>
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold text-center mt-4">Plataforma Médica</h1>
+      <RegistrarPaciente />
+      <RegistrarMedico />
     </div>
-  )
+  );
 }
 
 export default App;
