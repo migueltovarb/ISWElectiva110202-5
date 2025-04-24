71959abd18d57a7750538f70c49a8c236aa99b2d
Johan Delgado <tucorreo@ejemplo.com>
2025-04-23 22:51:40 -0500

    correcion archivos

--- GestionCitasMedicas/AppCitasMedicas/urls.py
+++ GestionCitasMedicas/AppCitasMedicas/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('registro-paciente/', views.registro_paciente, name='registro_paciente'),
     path('activar-cuenta/<int:uid>/<str:token>/', views.activar_cuenta, name='activar_cuenta'),
     path('registro-medico/', registrar_medico, name='registro_medico'),
-    #path('login-paciente/', views.login_paciente, name='login_paciente'),
+    path('login-paciente/', views.login_paciente, name='login_paciente'),
 ]
 
