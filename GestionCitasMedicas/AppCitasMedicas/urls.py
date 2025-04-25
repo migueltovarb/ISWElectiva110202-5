cac5ada0dc1071ffb235679b5277aae20cf2b981
Johan Delgado <tucorreo@ejemplo.com>
2025-04-25 01:11:15 -0500

    correcion rama

--- GestionCitasMedicas/AppCitasMedicas/urls.py
+++ GestionCitasMedicas/AppCitasMedicas/urls.py
@@ -11,10 +11,4 @@ urlpatterns = [
     path('registro-paciente/', views.registro_paciente, name='registro_paciente'),
     path('activar-cuenta/<int:uid>/<str:token>/', views.activar_cuenta, name='activar_cuenta'),
     path('registro-medico/', registrar_medico, name='registro_medico'),
-    path('login-paciente/', views.login_paciente, name='login_paciente'),
-    path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('medico/login/', LoginMedicoView.as_view(), name='login-medico'),
 ]
--- GestionCitasMedicas/AppCitasMedicas/views.py
+++ GestionCitasMedicas/AppCitasMedicas/views.py
@@ -10,26 +10,12 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
 from django.contrib.auth import authenticate
-import random
-import string
-from django.utils.decorators import method_decorator
-from django.contrib.auth.views import PasswordResetConfirmView
-from django.contrib.auth.forms import SetPasswordForm
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import authenticate
-from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
-from AppCitasMedicas.models import Medico
-from rest_framework.authtoken.models import Token
 
 
 @csrf_exempt
 def registro_paciente(request):
     if request.method == 'POST':
         data = json.loads(request.body)
-        print("Datos recibidos desde Postman:", data)
         nombre_completo = data.get('nombre_completo')
         email = data.get('email')
         telefono = data.get('telefono')
@@ -56,8 +42,6 @@ def registro_paciente(request):
         asunto = 'Activa tu cuenta'
         mensaje = f'Hola {nombre_completo},\n\nGracias por registrarte. Activa tu cuenta dando clic en el siguiente enlace:\n\n{enlace_activacion}'
         send_mail(asunto, mensaje, settings.DEFAULT_FROM_EMAIL, [email])
-
-        # (Aquí más adelante agregaremos el envío de correo de confirmación)
 
         return JsonResponse({'mensaje': 'Usuario registrado correctamente. Por favor, revisa tu correo para activar la cuenta.'})
 
@@ -122,75 +106,6 @@ def registrar_medico(request):
     
     return JsonResponse({'error': 'Método no permitido'}, status=405)
 
-class LoginMedicoView(APIView):
-    def post(self, request):
-        email = request.data.get('email')
-        password = request.data.get('password')
 
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            return Response({'error': 'Correo o contraseña incorrectos'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        user = authenticate(username=user.username, password=password)
-
-        if user is not None:
-            try:
-                medico = Medico.objects.get(user=user)
-            except Medico.DoesNotExist:
-                return Response({'error': 'Este usuario no está registrado como médico'}, status=status.HTTP_403_FORBIDDEN)
-
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({
-                'token': token.key,
-                'medico': {
-                    'nombre': medico.nombre_completo,
-                    'especialidad': medico.especialidad,
-                    'email': user.email
-                }
-            })
-        else:
-            return Response({'error': 'Correo o contraseña incorrectos'}, status=status.HTTP_401_UNAUTHORIZED)
-
-@csrf_exempt
-def login_paciente(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Formato JSON inválido'}, status=400)
-
-        email = data.get('email')
-        password = data.get('password')
-
-        if not email or not password:
-            return JsonResponse({'error': 'Email y contraseña son obligatorios'}, status=400)
-
-        try:
-            user = User.objects.get(email=email)
-        except User.DoesNotExist:
-            return JsonResponse({'error': 'Usuario no encontrado'}, status=404)
-
-        if not user.check_password(password):
-            return JsonResponse({'error': 'Contraseña incorrecta'}, status=400)
-
-        if not user.is_active:
-            return JsonResponse({'error': 'La cuenta no está activada. Revisa tu correo.'}, status=403)
-
-        return JsonResponse({'mensaje': 'Inicio de sesión exitoso'}, status=200)
-
-    return JsonResponse({'error': 'Método no permitido'}, status=405)
-
-@method_decorator(csrf_exempt, name='dispatch')
-class CustomPasswordResetConfirmView(PasswordResetConfirmView):
-    form_class = SetPasswordForm
-    success_url = '/api/restablecer/hecho/'
-
-    def form_valid(self, form):
-        form.save()
-        return JsonResponse({'mensaje': 'La contraseña ha sido restablecida exitosamente.'}, status=200)
-
-    def form_invalid(self, form):
-        return JsonResponse({'error': 'La contraseña no cumple los requisitos.'}, status=400)
 
 
