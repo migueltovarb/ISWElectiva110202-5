c5b3cdbae9b0517bf2d28948ce3e3ca92a98adb9
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-04-27 20:39:26 -0500

    pruebas

--- Creacion_Front_citasMedicas/vite.config.js
+++ Creacion_Front_citasMedicas/vite.config.js
@@ -5,7 +5,28 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
-    
-})
 
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.js",
+    coverage: {
+      provider: 'v8',
+      reporter: ["text", "html"],
+      statements: 80,
+      functions: 80,
+      lines: 80,
+      },
+      checkCoverage: true,
+    },
+    resolve:{
+      alias: {
+        src: "/src",
+      },
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    }
+
+  });
+
+    
 
