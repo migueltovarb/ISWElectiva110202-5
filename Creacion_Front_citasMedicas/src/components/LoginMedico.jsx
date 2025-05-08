7a35be80297bbf22821bc54186d3bcc9ea781750
Johan <delgadodelgad17@gmail.com>
2025-05-07 19:47:25 -0500

    creacion_login_medico

---
+++ Creacion_Front_citasMedicas/src/components/LoginMedico.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginMedico() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+}
