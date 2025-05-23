cb1a3309e7a08bd4384725374567feee7a8aeff9
Johan <delgadodelgad17mail.com>
2025-05-23 11:34:32 -0500

    crud

--- back/ListaTareas/ListaTareas/settings.py
+++ back/ListaTareas/ListaTareas/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'AppTareas'
+    'AppListaTreas',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
