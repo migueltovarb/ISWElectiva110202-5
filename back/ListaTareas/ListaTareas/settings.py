05eff99337c7b633bdfc9adce7e682b961241198
Johan <delgadodelgad17mail.com>
2025-05-23 12:08:36 -0500

    unacoma

--- back/ListaTareas/ListaTareas/settings.py
+++ back/ListaTareas/ListaTareas/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AppListaTreas',
-    'rest_framework'
-    'corsheaders',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
