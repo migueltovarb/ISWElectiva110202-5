b03dd9fd8d98ba48e4cd651f08daec83ac76b459
Johan <delgadodelgad17@gmail.com>
2025-05-26 17:37:11 -0500

    correccion front

--- Creacion_Front_citasMedicas/src/components/LoginPaciente.jsx
+++ Creacion_Front_citasMedicas/src/components/LoginPaciente.jsx
@@ -10,7 +10,7 @@ const LoginPaciente = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}api/paciente/login/`, {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}api/login-paciente/`, {
         email,
         password,
       });
