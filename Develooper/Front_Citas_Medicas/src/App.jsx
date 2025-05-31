5f18bc7f3db8a9ae7e6d2a8767f23c716647a019
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2025-05-30 23:11:40 -0500

    cofiig desing

--- Develooper/Front_Citas_Medicas/src/App.jsx
+++ Develooper/Front_Citas_Medicas/src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, BrowserRouter as Router, Routes, useNavigate } from 'react-router-dom';
+import RegistrarMedico from './components/DoctorRegister';
+import Login from './components/Login';
+import RegistrarPaciente from './components/PatientRegister';
+import HomePaciente from './components/HomePaciente';
+import HomeDoctor from './components/HomeDoctor';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function Landing() {
+  const navigate = useNavigate();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="min-h-screen bg-gradient-to-br from-gray-500 via-gray-950 to-gray-500 p-6 flex flex-col items-center justify-start">
+      <h1 className="text-4xl md:text-5xl text-white font-bold text-center mt-2 mb-4 drop-shadow-lg"> 🥼Plataforma Citas Médica🩺</h1>
+      <nav className="flex flex-col md:flex-row justify-center gap-4 mt-80">
+        <button
+          onClick={() => navigate("/auth/login/")}
+          className="flex items-center gap-2 bg-indigo-700 text-white px-6 py-3 rounded-xl hover:bg--600 transition-all duration-300 shadow-lg hover:scale-105"
+        >
+          Iniciar Sesión 
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <button
+          onClick={() => navigate("/auth/register/patient/")}
+          className="flex items-center gap-2 bg-indigo-700 text-white px-6 py-3 rounded-xl hover:bg-indigo-600 transition-all duration-300 shadow-lg hover:scale-105"
+        >
+          Registrar Paciente
+        </button>
+        <button
+          onClick={() => navigate("/auth/register/doctor/")}
+          className="flex items-center gap-2 bg-indigo-700 text-white px-6 py-3 rounded-xl hover:bg-indigo-600 transition-all duration-300 shadow-lg hover:scale-105"
+        >
+          Registrar Médico
+        </button>
+      </nav>
+    </div>
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/auth/login/" element={<Login />} />
+        <Route path="/auth/register/patient/" element={<RegistrarPaciente />} />
+        <Route path="/auth/register/doctor/" element={<RegistrarMedico />} />
+        <Route path="/appointments/create/" element={<HomePaciente />} />
+        <Route path="/appointments/doctor/" element={<HomeDoctor />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
